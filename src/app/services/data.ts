8cc38dfda749f0fda689f4f1699d720685d3a836
vikashkronline <vikashkronline@gmail.com>
2022-06-30 14:07:26 +0530

    1 file modified, 1 added

---
+++ src/app/services/data.ts
@@ -0,0 +1,10 @@
+export const users = [
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+  { id: 0, fname: 'Vikash', lname: 'Kumar', userId: 'vkumar' },
+];
